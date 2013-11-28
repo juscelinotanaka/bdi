e96507b9d65790fec530b2d25708736552d45701
Henrique Cavalcante <henrique.pcavalcante@gmail.com>
2013-11-28 18:00:29 -0400

    Teste com BD

--- tp1/controller/conexao.php
+++ tp1/controller/conexao.php
@@ -1,11 +1,15 @@
 <?php
+	
 	define('PGHOST','localhost');
 	define('PGPORT',5432);
-	define('PGDATABASE','bdi_tp1');
-	define('PGUSER', 'bdi');
-	define('PGPASSWORD', '123');
+	define('PGDATABASE','henriquecavalcante');
+	define('PGUSER', 'henriquecavalcante');
+	define('PGPASSWORD', '');
 	define('PGCLIENTENCODING','UNICODE');
 	define('ERROR_ON_CONNECT_FAILED','Sorry, can not connect the database server now!');
 	
 	pg_pconnect('host=' . PGHOST . ' port=' . PGPORT . ' dbname=' . PGDATABASE . ' user=' . PGUSER . ' password=' . PGPASSWORD);
+
+	
+
 ?>
--- tp1/controller/usuario-dao.php
+++ tp1/controller/usuario-dao.php
@@ -1,8 +1,13 @@
 <?php
-	include("controller/conexao.php");
-	include_once("model/usuario-class.php");
+	include("conexao.php");
+	//include_once("../model/usuario-class.php");
+
+	$qry = "INSERT INTO bd_tp1.usuario (nome, sobrenome, cpf, email, senha) VALUES ('HENRIQUE', 'HENRIQUE', 'HENRIQUE', 'HENRIQUE', 'HENRIQUE')";
+	pg_query($qry);
 	
 	function cadastrarUsuario($usuario){
+		$qry = 'INSERT INTO bd_tp1.usuario (null, "HENRIQUE", "HENRIQUE", "HENRIQUE", "HENRIQUE", "HENRIQUE")';
+		pg_query($qry);
 	}
 	
 	function consultarUsuario($usuario){
