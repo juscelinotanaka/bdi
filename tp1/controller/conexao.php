78ec3681a5b04c66a1168f6e2bff1c3077d8df34
Henrique Cavalcante <henrique.pcavalcante@gmail.com>
2013-11-28 21:29:28 -0400

    crud usuario
    cadastro e consultas prontas

--- tp1/controller/conexao.php
+++ tp1/controller/conexao.php
@@ -8,8 +8,8 @@
 	define('PGCLIENTENCODING','UNICODE');
 	define('ERROR_ON_CONNECT_FAILED','Sorry, can not connect the database server now!');
 	
-	pg_pconnect('host=' . PGHOST . ' port=' . PGPORT . ' dbname=' . PGDATABASE . ' user=' . PGUSER . ' password=' . PGPASSWORD);
+	$dbcon = pg_pconnect('host=' . PGHOST . ' port=' . PGPORT . ' dbname=' . PGDATABASE . ' user=' . PGUSER . ' password=' . PGPASSWORD);
 
-	
+	pg_set_client_encoding($dbcon, UTF-8);
 
 ?>
--- tp1/controller/usuario-dao.php
+++ tp1/controller/usuario-dao.php
@@ -1,16 +1,60 @@
 <?php
-	include("conexao.php");
-	//include_once("../model/usuario-class.php");
-
-	$qry = "INSERT INTO bd_tp1.usuario (nome, sobrenome, cpf, email, senha) VALUES ('HENRIQUE', 'HENRIQUE', 'HENRIQUE', 'HENRIQUE', 'HENRIQUE')";
-	pg_query($qry);
 	
+	include("conexao.php");
+	include_once("../model/usuario-class.php");
+
 	function cadastrarUsuario($usuario){
-		$qry = 'INSERT INTO bd_tp1.usuario (null, "HENRIQUE", "HENRIQUE", "HENRIQUE", "HENRIQUE", "HENRIQUE")';
-		pg_query($qry);
+		$qry = "INSERT INTO public.usuario ('".$usuario->getNome()."', '".$usuario->getSobrenome()."', '".$usuario->getCpf()."', '".$usuario->getEmail()."', '".$usuario->getSenha()."', '".$usuario->getApelido()."')";
+		$result = pg_query($qry);
+		
+		if(pg_affected_rows($result)>0){
+			return 1;
+		}
+		else{
+			return 0;
+		}
+		
 	}
 	
-	function consultarUsuario($usuario){
+	function consultarUsuario($id){
+		$qry = "SELECT * FROM public.usuario WHERE id =". $id ."";
+		$result = pg_query($qry);
+		
+		if (pg_num_rows($result) == 1){
+			$row = pg_fetch_object($result);
+			
+			$usuario = new Usuario();
+			
+			$usuario->setId($row->id);
+			$usuario->setNome($row->nome);
+			$usuario->setSobrenome($row->apelido);
+			$usuario->setCpf($row->cpf);
+			$usuario->setEmail($row->email);
+			$usuario->setApelido($row->apelido);
+			
+			return $usuario;
+		}
+	}
+	
+	function listarUsuarios(){
+		$qry = "SELECT * FROM public.usuario";
+		$result = pg_query($qry);
+		
+		while($row = pg_fetch_object($result)){
+			
+			$usuario = new Usuario();
+			
+			$usuario->setId($row->id);
+			$usuario->setNome($row->nome);
+			$usuario->setSobrenome($row->apelido);
+			$usuario->setCpf($row->cpf);
+			$usuario->setEmail($row->email);
+			$usuario->setApelido($row->apelido);
+			
+			$usuarios[] = $usuario;
+		}
+		
+		return $usuarios;
 	}
 	
 ?>
