eace8f087997ae3e047d797ed01ef59c81cadee1
Henrique Cavalcante <henrique.pcavalcante@gmail.com>
2013-12-02 17:31:00 -0400

    uau

--- tp1/index.php
+++ tp1/index.php
@@ -38,7 +38,7 @@
                 	<div class="tituloGrupo">Recomendados para Você</div>
                     <? foreach ($recomendacoes as $prod) { ?>
                     <div class="produto">
-                        <div class="image"><a href="<? echo SYSURL ?>produto/detalhes.php?id=<? echo $prod->getId(); ?>"><img src="nbs/<?php echo $prod->getImagem();?>" alt="Coffee Cups"></a></div>
+                        <div class="image"><a href="<? echo SYSURL ?>produto/detalhes.php?id=<? echo $prod->getId(); ?>"><img src="nbs/<?php echo $prod->getImagem();?>" width="130" alt="Coffee Cups"></a></div>
                         <div class="name"><a href="<? echo SYSURL ?>produto/detalhes.php?id=<? echo $prod->getId(); ?>"><? echo $prod->getNome(); ?></a></div>
                         <div class="price"><? echo $prod->getPreco(); ?></div>
                         <div class="cart"><a href="<? echo SYSURL ?>produto/detalhes.php?id=<? echo $prod->getId(); ?>" class="button"><span>Detalhes</span></a></div>
