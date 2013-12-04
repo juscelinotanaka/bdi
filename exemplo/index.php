ef5fd41af83eb9cffcb1265ab4bd1363c8ca17fc
sarmenufam <sarmenufam@gmail.com>
2013-12-03 23:37:15 -0400

    Criação do Exemplo da Aula

---
+++ exemplo/index.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Cadastro de Usuário</title>
+<style type="text/css">
+.esquerda {
+	width:100px;
+}
+
+a:link {
+	text-decoration: none;
+}
+a:visited {
+	text-decoration: none;
+}
+a:hover {
+	text-decoration: none;
+}
+a:active {
+	text-decoration: none;
+}
+</style>
+</head>
+
+
+
+<body>
+
+<form>
+	<div>Nome:</div> <input type="text" name="nome"><br>
+    <div>Sobrenome:</div> <input type="text" name="sobrenome"><br>
+    <div>CPF:</div> <input type="text" name="cpf"><br>
+    <div>Email:</div> <input type="email" name="email"><br>
+    <div>Senha:</div> <input type="password" name="senha"><br>
+    <div>Apelido:</div> <input type="text" name="apelido">
+    <br><br>
+    <input type="submit" value="Cadastrar">
+</form>
+
+</body>
+</html>
