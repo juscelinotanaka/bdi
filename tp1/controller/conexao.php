259593d6295fdf9a47f834e36b7dc0b03549f89f
sarmenufam <sarmenufam@gmail.com>
2013-12-12 15:06:50 -0400

    Banco Web - Descomentar o local

--- tp1/controller/conexao.php
+++ tp1/controller/conexao.php
@@ -1,5 +1,5 @@
 <?php
-	
+	/*
 	define('PGHOST','localhost');
 	define('PGPORT',5432);
 	define('PGDATABASE','bdi_tp1');
@@ -7,7 +7,17 @@
 	define('PGPASSWORD', '123');
 	define('PGCLIENTENCODING','UNICODE');
 	define('ERROR_ON_CONNECT_FAILED','Sorry, can not connect the database server now!');
+	/* */
 	
+	/* */
+	define('PGHOST','10.208.200.15');
+	define('PGPORT',5432);
+	define('PGDATABASE','tp01_2013_01');
+	define('PGUSER', 'juscelino');
+	define('PGPASSWORD', '123456');
+	define('PGCLIENTENCODING','UNICODE');
+	define('ERROR_ON_CONNECT_FAILED','Sorry, can not connect the database server now!');
+	/* */
 	
 	
 	$db = pg_pconnect('host=' . PGHOST . ' port=' . PGPORT . ' dbname=' . PGDATABASE . ' user=' . PGUSER . ' password=' . PGPASSWORD);
